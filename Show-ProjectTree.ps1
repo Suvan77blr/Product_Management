1157215738aabb9b1d022cc1f7122c13c2c98bf1
Suvan77blr <sbumsrit61blr@gmail.com>
2025-05-29 20:39:25 +0530

    Added Show-ProjectTree.ps1 script
    TO display the project tree structure .. accounting for default
    directories to exclude and MaxDepth of 5.
    Starts by default from the current directory.
    
    The fields can be adjusted for custom needs.
    
    By editing the script, custom changes can be applied permanently.

---
+++ Show-ProjectTree.ps1
@@ -0,0 +1,52 @@
+# PS script to:
+#   - display a tree view of your project folder, 
+#   - with options to exclude specific directories and limit depth for a cleaner overview.
+
+# Usage:
+#    DEFAULT: ".\Show-ProjectTree.ps1"
+#       => traverses from Current Folder to 'MaxDepth' of 5, excluding node_modules & '.git' folders.
+# 
+#   CUSTOM USAGE Example:
+#   .\Show_Project_Tree.ps1 -Path . -ExcludeDirs "node_modules", ".git", "bin" -MaxDepth 1
+
+# Default values.
+param (
+    [string]$Path = ".",
+    [string[]]$ExcludeDirs = @("node_modules", ".git"),
+    [int]$MaxDepth = 5
+)
+
+function Show-Tree {
+    param (
+        [string]$Path = ".",
+        [int]$Level = 0,
+        [string[]]$ExcludeDirs = @(),
+        [int]$MaxDepth = 1
+    )
+
+    if ($Level -ge $MaxDepth) {
+        return
+    }
+
+    $indent = " " * ($Level * 4)
+
+    Get-ChildItem -Path $Path | Where-Object {
+        if ($_.PSIsContainer) {
+            $ExcludeDirs -notcontains $_.Name
+        }
+        else {
+            $true
+        }
+    } | ForEach-Object {
+        if ($_.PSIsContainer) {
+            Write-Output "$indent|-- $_"
+            Show-Tree -Path $_.FullName -Level ($Level + 1) -ExcludeDirs $ExcludeDirs -MaxDepth $MaxDepth
+        }
+        else {
+            Write-Output "$indent|-- $_"
+        }
+    }
+}
+
+# Call the function with user-supplied arguments
+Show-Tree -Path $Path -ExcludeDirs $ExcludeDirs -MaxDepth $MaxDepth
